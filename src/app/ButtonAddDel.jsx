af18201043675f70a859c231f3058f164cb6a39c
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2021-05-23 20:35:01 +0300

    Delete src directory

--- src/app/ButtonAddDel.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default class ButtonAddDel extends React.Component {
-  render() {
-    const { label, handleClick } = this.props;
-    return <button onClick={handleClick}>{label}</button>;
-  }
-}
--- src/app/UserAvatar.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserAvatar(props) {
-  return <img src={props.src} alt="usrAvatar" />;
-}
